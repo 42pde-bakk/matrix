d98c43f48598158a6888a55e7d91893b29f3ee1f
pde-bakk <pde-bakk@student.codam.nl>
2022-10-10 20:02:46 +0200

    feq ex11

--- srcs/ex11.cpp
+++ srcs/ex11.cpp
@@ -20,21 +20,21 @@ int main() {
 		 {-1.0, 1.0}};
 	determinant = u.determinant();
 	std::cout << determinant << "\n";
-	assert(determinant == 0.0);
+	assert(feq(determinant, 0.0));
 
 	u = {{2.0, 0.0, 0.0},
 		 {0.0, 2.0, 0.0},
 		 {0.0, 0.0, 2.0}};
 	determinant = u.determinant();
 	std::cout << determinant << "\n";
-	assert(determinant == 8.0);
+	assert(feq(determinant, 8.0));
 
 	u = {{8.0, 5.0, -2.0},
 		 {4.0, 7.0, 20.0},
 		 {7.0, 6.0, 1.0}};
 	determinant = u.determinant();
 	std::cout << determinant << "\n";
-	assert(determinant == -174.0);
+	assert(feq(determinant, -174.0));
 
 	u = {{8.0, 5.0, -2.0, 4.0},
 		 {4.0, 2.5, 20.0, 4.0},
@@ -42,7 +42,7 @@ int main() {
 		 {28.0, -4.0, 17.0, 1.0}};
 	determinant = u.determinant();
 	std::cout << determinant << "\n";
-	assert(determinant == 1032.0);
+	assert(feq(determinant, 1032.0));
 
 	std::cout << "All done with ex11\n";
 	return (0);
