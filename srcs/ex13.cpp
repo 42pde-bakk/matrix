7c03c3e7e3f52067ad17175d9839793f7f7b1273
pde-bakk <pde-bakk@student.codam.nl>
2022-10-10 20:01:47 +0200

    added asserts

--- srcs/ex13.cpp
+++ srcs/ex13.cpp
@@ -21,12 +21,14 @@ int main() {
 		 {0.0, 0.0, 1.0}};
 	rank = u.rank();
 	std::cout << "rank is " << rank << "\n";
+	assert(rank == 3);
 
 	u = {{1.0, 2.0, 0.0, 0.0},
 		 {2.0, 4.0, 0.0, 0.0},
 		 {-1.0, 2.0, 1.0, 1.0}};
 	rank = u.rank();
 	std::cout << "rank is " << rank << "\n";
+	assert(rank == 2);
 
 	u = {{8.0, 5.0, -2.0},
 		 {4.0, 7.0, 20.0,},
@@ -34,6 +36,7 @@ int main() {
 		 {21.0, 18.0, 7.0}};
 	rank = u.rank();
 	std::cout << "rank is " << rank << "\n";
+	assert(rank == 3);
 
 	std::cout << "All done with ex13\n";
 	return (0);
