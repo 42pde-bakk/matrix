c37e0d058d5983ab7480814ce77fb254f0007ad2
pde-bakk <pde-bakk@student.codam.nl>
2022-10-17 22:55:59 +0200

    working on ex15/01.cpp

--- srcs/ex15/ex01.cpp
+++ srcs/ex15/ex01.cpp
@@ -3,12 +3,11 @@
 //
 
 #include <complex>
-using namespace std::complex_literals;
-
 #include "Vector.hpp"
 #include <iostream>
 #include <cstdio>
 #include <cassert>
+using namespace std::complex_literals;
 
 template <typename K>
 ft::Vector<K>	linear_combination(const std::vector<ft::Vector<K>>& u, const std::vector<K>& coefs) {
@@ -28,14 +27,14 @@ ft::Vector<K>	linear_combination(const std::vector<ft::Vector<K>>& u, const std:
 }
 
 int main() {
-	auto e1 = ft::Vector<float>({1.0, 0.0, 0.0});
-	auto e2 = ft::Vector<float>({0.0, 1.0, 0.0});
-	auto e3 = ft::Vector<float>({0.0, 0.0, 1.0});
+	auto e1 = ft::Vector<std::complex<float>>({1.0if, 0.0if, 0.0if});
+	auto e2 = ft::Vector<std::complex<float>>({0.0if, 1.0if, 0.0if});
+	auto e3 = ft::Vector<std::complex<float>>({0.0if, 0.0if, 1.0if});
 
-	auto v1 = ft::Vector<float>({1.0, 2.0, 3.0});
-	auto v2 = ft::Vector<float>({0.0, 10.0, -100.0});
+	auto v1 = ft::Vector<std::complex<float>>({1.0if, 2.0if, 3.0if});
+	auto v2 = ft::Vector<std::complex<float>>({0.0if, 10.0if, -100.0if});
 
-	std::vector<float>	coefs1 = {10.0, -2.0, 0.5};
+	std::vector<std::complex<float>>	coefs1 = {10.0if, -2.0if, 0.5if};
 	auto lin_comb1 = linear_combination({e1, e2, e3}, coefs1);
 	for (size_t i = 0; i < lin_comb1.size(); i++) {
 		std::cout << lin_comb1[i] << ' ';
@@ -43,7 +42,7 @@ int main() {
 	std::cout << "\n";
 	assert(lin_comb1[0] == 10.0 && lin_comb1[1] == -2.0 && lin_comb1[2] == 0.5);
 
-	std::vector<float> coefs2 = {10.0, -2.0};
+	std::vector<std::complex<float>> coefs2 = {10.0, -2.0};
 	auto lin_comb2 = linear_combination({v1, v2}, coefs2);
 	assert(lin_comb2[0] == 10.0 && lin_comb2[1] == 0.0);
 
