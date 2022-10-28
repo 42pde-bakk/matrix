4f51ae978cc672d2698c308e9a091939dea95a08
pde-bakk <pde-bakk@student.codam.nl>
2022-10-28 13:22:44 +0200

    hopefully fixed my ex14 code

--- srcs/ex14.cpp
+++ srcs/ex14.cpp
@@ -11,30 +11,28 @@ namespace ft {
 	 * https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/building-basic-perspective-projection-matrix
 	 */
 	Matrix<double>	projection(double fov, double ratio, double near, double far) {
-		auto	out = Matrix<double>::identity(4);
-		auto	radians = tan(fov / 2);
+		auto	radians = tan((fov / 2) * (M_PI / 180));
 		double	scale = 1.0 / radians;
 
-		out[0][0] = scale / ratio;
-		out[1][1] = scale;
-		out[2][2] = -(far) / (far - near);
-		out[2][3] = -1;
-		out[3][2] = -(far * near) / (far - near);
-
+		return Matrix<double>({
+				{scale / ratio, 0.0, 0.0, 0.0},
+				{0.0, scale, 0.0, 0.0},
+				{0.0, 0.0, -far / (far - near), -1.0},
+				{0.0, 0.0, -(far * near) / (far - near), 0.0}
+		});
 		/*
 		 * s/r	0	0	0
 		 * 0	s	0	0
 		 * 0	0	A	-1
 		 * 0	0	B	0
 		 */
-		return (out);
 	}
 }
 
 int main() {
 	ft::Matrix<double> u;
 
-	u = ft::projection(90, 1, 0.1, 10);
+	u = ft::projection(90, 1.0, 1, 100);
 
 	std::cerr << u << "\n";
 	u.to_proj(std::cout);
